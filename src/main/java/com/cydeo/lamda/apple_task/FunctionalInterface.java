10cdf4039ff668ec314a0d7e8f8ac79a246c222d
oleksiikoval <lexi4.koval@gmail.com>
2023-01-26 08:05:57 -0500

    Test different scenarios

--- src/main/java/com/cydeo/lamda/apple_task/FunctionalInterface.java
+++ src/main/java/com/cydeo/lamda/apple_task/FunctionalInterface.java
@@ -1,5 +1,6 @@
 package com.cydeo.lamda.apple_task;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -18,6 +19,11 @@ public class FunctionalInterface {
         System.out.println("*****************CONSUMER***************");
         Consumer<Integer> display = i-> System.out.println("Consumer " + i);
         display.accept(20);
+
+        System.out.println("*****************BI CONSUMER***************");
+
+        BiConsumer<Integer,Integer> addTwo = (x,y)-> System.out.println(x+y);
+        addTwo.accept(5,4);
 
 
 
